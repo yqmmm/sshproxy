af9ebdb770075484a3193413f47ce10c53a60585
Qianmian Yu <im.qianmian.yu@gmail.com>
2022-04-08 19:17:30 +0800

    Add nix flake.

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "SSH Proxy";
+
+  outputs = { self, nixpkgs }:
+    let
+
+      # to work with older version of flakes
+      lastModifiedDate = self.lastModifiedDate or self.lastModified or "19700101";
+
+      # Generate a user-friendly version number.
+      version = builtins.substring 0 8 lastModifiedDate;
+
+      # System types to support.
+      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+
+      # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+
+      # Nixpkgs instantiated for supported system types.
+      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+
+    in
+    {
+
+      # Provide some binary packages for selected system types.
+      packages = forAllSystems (system:
+        let
+          pkgs = nixpkgsFor.${system};
+        in
+        {
+          sshproxy = pkgs.buildGoModule {
+            pname = "sshproxy";
+            inherit version;
+            src = ./.;
+
+            vendorSha256 = "sha256-43XyJRBfLz5emz9Z+BVgpyJ8uOOv+rKk2whx1I7UMPY=";
+          };
+        });
+
+      defaultPackage = forAllSystems (system: self.packages.${system}.sshproxy);
+    };
+}
