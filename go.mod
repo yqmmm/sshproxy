a6df0a8c112a84ca4892c5814a130f4ec86391e4
Qianmian Yu <im.qianmian.yu@gmail.com>
2022-04-08 18:51:17 +0800

    Initial commit

---
+++ go.mod
@@ -0,0 +1,13 @@
+module sshproxy
+
+go 1.18
+
+require (
+	github.com/armon/go-socks5 v0.0.0-20160902184237-e75332964ef5
+	golang.org/x/crypto v0.0.0-20220331220935-ae2d96664a29
+)
+
+require (
+	golang.org/x/net v0.0.0-20211112202133-69e39bad7dc2 // indirect
+	golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"io/ioutil"
+	"log"
+	"net"
+
+	"github.com/armon/go-socks5"
+	"golang.org/x/crypto/ssh"
+)
+
+var privateKeyFlag = flag.String("privateKey", "", "")
+var hostFlag = flag.String("host", "", "")
+
+func main() {
+	flag.Parse()
+
+	privateBytes, err := ioutil.ReadFile(*privateKeyFlag)
+	if err != nil {
+		log.Fatalf("Failed to read private key %v: %v", *privateKeyFlag, err)
+	}
+
+	signer, err := ssh.ParsePrivateKey(privateBytes)
+	if err != nil {
+		log.Fatal("Failed to parse private key: ", err)
+	}
+
+	config := &ssh.ClientConfig{
+		User: "yqm",
+		Auth: []ssh.AuthMethod{
+			ssh.PublicKeys(signer),
+		},
+		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
+	}
+
+	client, err := ssh.Dial("tcp", *hostFlag, config)
+	if err != nil {
+		log.Fatal("Failed to dial: ", err)
+	}
+	defer client.Close()
+
+	conf := &socks5.Config{
+		Dial: func(ctx context.Context, network, addr string) (net.Conn, error) {
+			log.Printf("Connectiont to %v with %v", addr, network)
+			conn, err := client.Dial(network, addr)
+			if err != nil {
+				log.Printf("Failed to to SSH Dial: %v", err)
+			}
+			return conn, err
+		},
+	}
+	server, err := socks5.New(conf)
+	if err != nil {
+		log.Fatal("Failed to create SOCKS5 server: ", err)
+	}
+
+	if err := server.ListenAndServe("tcp", "127.0.0.1:7788"); err != nil {
+		panic(err)
+	}
+}
